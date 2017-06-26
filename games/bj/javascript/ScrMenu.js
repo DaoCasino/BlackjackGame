6b8d249d0c0ed80d548c4ce0838b49bca50511db
J0xFlash <joxsea@mail.ru>
2017-06-26 18:34:41 +0200

    test

--- games/bj/javascript/ScrMenu.js
+++ games/bj/javascript/ScrMenu.js
@@ -48,6 +48,7 @@ ScrMenu.prototype.init = function() {
 	tfSpeed.y = btnSpeed.y - tfSpeed.height/2;
 	this.addChild(tfSpeed);
 	
+	
 	var str1 = "This game is a proof of concept and intended for test purposes. It is based on experimental software.";
 	var str2 = "In no respect shall this game or its authors incur any liability for the loss of ether.";
 	var str3 = "Players who access this game from other jurisdictions do so at their own volition and are responsible for compliance with local law.";
