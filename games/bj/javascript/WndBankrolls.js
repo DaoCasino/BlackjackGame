f2ef82e1a7f4995e70827e6d3a35f0fd7d7d2c1f
J0xFlash <joxsea@mail.ru>
2017-08-02 12:40:46 +0200

    Fixed bankrollers

--- games/bj/javascript/WndBankrolls.js
+++ games/bj/javascript/WndBankrolls.js
@@ -141,7 +141,7 @@ WndBankrolls.prototype.show = function() {
 	var load = false;
 	
 	// console.log("showBankrolls:", arAdr);
-	ar = ["0xe26b3678fef015f3122e78f9d85b292ce45975b1"];
+	arAdr = ["0xe26b3678fef015f3122e78f9d85b292ce45975b1"];
 	this.loading.visible = (arAdr.length == 0);
 	
 	if(arAdr.length == 0){
